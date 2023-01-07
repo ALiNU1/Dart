4473574a7f3843f0177d2bccb87b1cfdde7083a5
alinu1 <uwu1343@gmail.com>
2023-01-08 04:21:52 +0600

    variable

---
+++ bin/dart_application.dart
@@ -0,0 +1,26 @@
+var num = 0;
+var world = 'Hello';
+
+void main(List<String> arguments) {
+
+  var point = 4.5;
+  // point = point+1;
+  // point++;
+  
+  // int num = 0;
+  String some;
+  some = 'Hello';
+
+  bool isHappy;
+  isHappy = true;
+
+  print('Переменная: ${point+0.5}. Еще что-то: ${some.toUpperCase()}');
+
+  dynamic dynamisData = 5;
+  // dynamisData = 'world';
+  // dynamisData = false;
+  print(dynamisData);
+
+  var a = 2,b=3;
+  print(a+b);
+}
